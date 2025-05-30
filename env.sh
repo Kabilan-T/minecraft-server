e29f821a7379a70ae723dd048edae70e5757cb6c
Kabilan Tamilmani <kabilan.tamilmani@dfki.de>
2025-05-30 17:50:00 +0200

    log file for each session

--- env.sh
+++ env.sh
@@ -2,8 +2,10 @@
 
 BASE_DIR="$(pwd)"
 
+export BASE_DIR
 export SESSION_NAME="blueWorld_server_session"
 export BEDROCK_SERVER_PATH="$BASE_DIR/bedrock-server"
 export BACKUP_DIR="$BASE_DIR/backup"
+export LOG_DIR="$BASE_DIR/logs"
 
 echo "Done"
--- start_blueWorld.bash
+++ start_blueWorld.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -z "$BEDROCK_SERVER_PATH" ] || [ -z "$SESSION_NAME" ]; then
+if [ -z "$BEDROCK_SERVER_PATH" ] || [ -z "$SESSION_NAME" ] || [ -z "$LOG_DIR" ]; then
   echo "Please source env.sh before running this script."
   exit 1
 fi
@@ -13,12 +13,17 @@ sleep 1
 # === Forwarding session outputs to log ===
 tmux send-keys -t ${SESSION_NAME} "cd ${BEDROCK_SERVER_PATH}" Enter
 sleep 1
-tmux pipe-pane -t ${SESSION_NAME} -o "cat > blueWorld_server.log"
+mkdir -p "${LOG_DIR}"
+timestamp=$(date +"%Y%m%d_%H%M%S")
+log_file="${LOG_DIR}/blueWorld_server_${timestamp}.log"
+touch "${log_file}"
+tmux pipe-pane -t ${SESSION_NAME}:0.0 -o "cat > ${log_file}"
 
 # === Launching bedrock server ===
 tmux send-keys -t ${SESSION_NAME} 'LD_LIBRARY_PATH=. ./bedrock_server' Enter
 
 
 echo "blueWorld server running..."
+echo "log file : ${log_file}"
 echo "Use the following command to attach to tmux session:"
 echo "  tmux attach -t ${SESSION_NAME}"
